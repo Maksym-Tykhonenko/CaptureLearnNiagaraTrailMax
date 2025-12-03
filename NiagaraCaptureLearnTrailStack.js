7709a648f54ad5dc9fd67c4f98e5e52f05693450
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-12-03 16:49:23 +0200

    fin

--- NiagaraCaptureLearnTrailStack.js
+++ NiagaraCaptureLearnTrailStack.js
@@ -333,7 +333,7 @@ const TrailNavigator = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2025-11-29T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-12-05T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
